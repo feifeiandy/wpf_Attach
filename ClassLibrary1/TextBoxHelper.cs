b26f142215ec12a8b435ae4b724ea8b086bb08d8
feifeiandy <feifeiandy@outlook.com>
2016-05-20 15:20:54 +0800

    t

---
+++ ClassLibrary1/TextBoxHelper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ClassLibrary1
+{
+    public class TextBoxHelper
+    {
+        //需要添加的程序集
+        //PresentationCore presentationFrameWork Xaml
+
+        public static string GetMyTxt(DependencyObject obj)
+        {
+            return (string)obj.GetValue(MyTxtProperty);
+        }
+
+        public static void SetMyTxt(DependencyObject obj, string value)
+        {
+            obj.SetValue(MyTxtProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for MyTxt.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty MyTxtProperty =
+            DependencyProperty.RegisterAttached("MyTxt", typeof(string), typeof(TextBoxHelper), new PropertyMetadata("xxx",TextChanged));
+       
+        // "xxx"  后面回调方法
+        private static void TextChanged(DependencyObject obj, DependencyPropertyChangedEventArgs e)
+        {
+            TextBox element = obj as TextBox;
+            if (element!=null)
+            {
+                element.Text = e.NewValue.ToString();
+            }
+        }
+
+               
+    }
+}
---
+++ WpfApplication2/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using ClassLibrary1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication2
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(TextBoxHelper.GetMyTxt(this.txt1));
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //TextBoxHelper.SetMyTxt(this.txt1, "www.bai.com");
+        }
+    }
+}
