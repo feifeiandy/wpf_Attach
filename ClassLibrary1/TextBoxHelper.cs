075c7066a710d8f6cb03047f10e8f2eff181601f
feifeiandy <feifeiandy@outlook.com>
2016-07-29 10:07:27 +0800

    s

--- ClassLibrary1/TextBoxHelper.cs
+++ ClassLibrary1/TextBoxHelper.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace ClassLibrary1
 {
@@ -12,7 +13,6 @@ namespace ClassLibrary1
     {
         //需要添加的程序集
         //PresentationCore presentationFrameWork Xaml
-
         public static string GetMyTxt(DependencyObject obj)
         {
             return (string)obj.GetValue(MyTxtProperty);
@@ -24,17 +24,20 @@ namespace ClassLibrary1
         }
 
         // Using a DependencyProperty as the backing store for MyTxt.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty MyTxtProperty =
-            DependencyProperty.RegisterAttached("MyTxt", typeof(string), typeof(TextBoxHelper), new PropertyMetadata("xxx",TextChanged));
+           public static readonly DependencyProperty MyTxtProperty =
+           DependencyProperty.RegisterAttached("MyTxt", typeof(string), typeof(TextBoxHelper), new PropertyMetadata("xxx", TextChanged));
        
         // "xxx"  后面回调方法
+        // 该事件的作用是将附加属性和控件的属性关联起来，如果此处的附加属元数据不添加事件那么就要在模板里面关联
+        //<TextBox Background="Green" Text="{TemplateBinding local:TextBoxHelper.MyTxt}"></TextBox>
         private static void TextChanged(DependencyObject obj, DependencyPropertyChangedEventArgs e)
         {
             TextBox element = obj as TextBox;
-            if (element!=null)
+            if (element != null)
             {
                 element.Text = e.NewValue.ToString();
             }
+            element.Foreground = new SolidColorBrush(Colors.Red);
         }
     }
 }
